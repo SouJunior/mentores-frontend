5e4e9853070e87fba3e1d46968290e3363193b02
Patrickresplandes <patrickresplandes87@gmail.com>
2023-04-06 19:48:23 -0300

    Criação de box-cadastro e componentes para uso do cadastro

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
-
-
 const nextConfig = {
   reactStrictMode: true,
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')],
-    },
 }
 
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,8 +21,11 @@ export default function Home() {
             <code className={styles.code}>src/pages/index.js</code>
           </p>
           <div>
-          <Link href="/Cadastro">
-            
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               By{' '}
               <Image
                 src="/vercel.svg"
@@ -33,9 +35,8 @@ export default function Home() {
                 height={24}
                 priority
               />
-              </Link>
+            </a>
           </div>
-          
         </div>
 
         <div className={styles.center}>
