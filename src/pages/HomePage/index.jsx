3838f8641601244b10a49462ebbbf144835f9223
sirdav1d <ddavid.diniz@gmail.com>
2023-05-02 03:35:44 -0300

    refactor: :art: code standardization

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import HeroSection from '../../components/organisms/HeroSection';
+export default function HomePage() {
+	return (
+		<div>
+			<HeroSection />
+		</div>
+	);
+}
---
+++ src/pages/_app.jsx
@@ -0,0 +1,45 @@
+import { theme } from '@/styles/theme';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '@/styles/GlobalStyle';
+
+export default function App({ Component, pageProps }) {
+	return (
+		<ThemeProvider theme={theme}>
+			<>
+				<Head>
+					<title>Sou Junior | Mentoria Online</title>
+					<meta
+						name='viewport'
+						content='width=device-width, initial-scale=1'
+					/>
+					<meta
+						name='description'
+						content='Portal oficial da Sou Junior para a comunicação entre mentores e profissionais que estejam ingressando na área de tecnonologia'
+					/>
+
+					<link
+						rel='preconnect'
+						href='https://fonts.googleapis.com'
+					/>
+					<link
+						rel='preconnect'
+						href='https://fonts.gstatic.com'
+						crossOrigin
+					/>
+					<link
+						href='https://fonts.googleapis.com/css2?family=Radio+Canada:wght@400;500;600;700&display=swap'
+						rel='stylesheet'
+					/>
+
+					<link
+						rel='icon'
+						href='/public/favicon.ico'
+					/>
+				</Head>
+				<GlobalStyle />
+				<Component {...pageProps} />
+			</>
+		</ThemeProvider>
+	);
+}
