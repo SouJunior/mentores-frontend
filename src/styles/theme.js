099ba8e19cfc48766dcd13e430676ab8f886ef6d
sirdav1d <ddavid.diniz@gmail.com>
2023-05-02 03:29:53 -0300

    feat: :sparkles: imtroduces styled-components

---
+++ src/styles/theme.js
@@ -0,0 +1,25 @@
+export const colors = {
+	blue: {
+		300: '#4498EE',
+		500: '#1165BA',
+		700: '#0D4C8C',
+	},
+	black: '#090909',
+	white: '#F9F9F9',
+	gray: '#5D5F5D',
+	opacityGray: 'rgba(197, 199, 197, 0.56)',
+	placeholder: '#C5C7C5',
+};
+
+export const fontSizes = {
+	xl: '40px',
+	lg: '24px',
+	md: '18px',
+	sm: '16px',
+	xs: '14px',
+};
+
+export const theme = {
+	colors,
+	fontSizes,
+};
