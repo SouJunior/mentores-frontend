de6b16cbba451710df2e3104a5e569b1197a07a3
abelardojr0 <abelardo.junior.181@gmail.com>
2023-06-07 00:34:01 -0300

    feat: criação dos modais de Termos de uso e o de Politicas de privacidade

--- src/components/atoms/FormRegister/style.js
+++ src/components/atoms/FormRegister/style.js
@@ -76,37 +76,44 @@ export const ModalHash = styled.p`
 `;
 
 export const ModalBox = styled.div`
-  max-height: 400px;
   overflow: auto;
   background-color: white;
   width: 100%;
   padding: 24px;
   margin-top: 30px;
+  font-family: "Radio Canada";
+  &::-webkit-scrollbar {
+    width: 15px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #5d5f5d;
+    border-radius: 100px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: rgba(215, 217, 215, 0.5);
+  }
 `;
 
 export const ModalBoxTitulo = styled.h2`
-  width: 521px;
-  height: 24px;
-  font-family: "Radio Canada";
   font-style: normal;
   font-weight: 700;
   font-size: 16px;
   line-height: 150%;
   color: #1165ba;
+  margin: 15px 0;
 `;
 
 export const ModalBoxSubTitulo = styled.h2`
-  font-family: "Radio Canada";
   font-style: normal;
   font-weight: bold;
   font-size: 14px;
   line-height: 150%;
   color: #000000;
+  margin-top: 15px;
 `;
 export const ModalBoxParagrafo = styled.p`
-  width: 521px;
-  height: 2289px;
-  font-family: "Radio Canada";
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
