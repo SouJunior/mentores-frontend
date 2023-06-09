c0c6f182e6590f931616b71a7f1d7d3f590c1ea8
sirdav1d <ddavid.diniz@gmail.com>
2023-06-09 17:46:35 -0300

    fix: :bug: problemas na renderiação do modal de confirmação de e-mail

--- src/components/molecules/ModalEmail/index.jsx
+++ src/components/molecules/ModalEmail/index.jsx
@@ -1,35 +1,37 @@
-import ModalComponent from "@/components/atoms/Modal";
-import { ContainerModal, ImageEmail, Message, TitleModal, Hash, FooterModal } from "./style";
-import Image from "next/image";
+import ModalComponent from '@/components/atoms/Modal';
+import {
+	ContainerModal,
+	ImageEmail,
+	Message,
+	TitleModal,
+	Hash,
+	FooterModal,
+} from './style';
+import Image from 'next/image';
 
-
-export default function ModalEmail ({open, onClose, children, height}) {
-
-    return (
-        <ModalComponent
-        open={open}
-        onClose={onClose}
-        children={children}
-        height={height}
-        >
-            <ContainerModal>
-                <TitleModal>Cheque seu email</TitleModal>
-                <ImageEmail
-                src="images/ConfirmEmail.png"
-                />
-                <Message>Enviamos um email para você de confirmação.</Message>
-                <FooterModal>
-
-                <Image
-                src={'logos/LogoSJ.svg'}
-                width={108}
-                height={17}                
-                />
-                <Hash>#MovimentoSouJunior</Hash>
-                </FooterModal>
-            </ContainerModal>
-        </ModalComponent>
-    )
-    
-
+export default function ModalEmail({ open, onClose, height }) {
+	return (
+		<ModalComponent
+			open={open}
+			onClose={onClose}
+			height={height}>
+			<ContainerModal>
+				<TitleModal>Cheque seu email</TitleModal>
+				<ImageEmail
+					src='images/ConfirmEmail.png'
+					alt='Garoto mexendo em nootebook'
+				/>
+				<Message>Enviamos um email para você de confirmação.</Message>
+				<FooterModal>
+					<Image
+						src={'logos/LogoSJ.svg'}
+						alt='logo da SouJunior'
+						width={108}
+						height={17}
+					/>
+					<Hash>#MovimentoSouJunior</Hash>
+				</FooterModal>
+			</ContainerModal>
+		</ModalComponent>
+	);
 }
