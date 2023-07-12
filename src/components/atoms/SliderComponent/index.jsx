873699013ceafc9aae89d78f00afc9b6941f175a
luizpbello <luizpaulopinto25@gmail.com>
2023-07-11 23:05:42 -0300

    feat: Fazendo carousel com swiper

--- src/components/atoms/SliderComponent/index.jsx
+++ src/components/atoms/SliderComponent/index.jsx
@@ -1,32 +1,40 @@
-import Carousel from 'better-react-carousel';
-import React from 'react';
-import { ArrowLeft, ArrowRight } from './style';
-const SliderComponent = ({
-	children,
-	cols,
-	rows,
-	autoplay,
-	gap,
-	arrowLeft,
-	arrowRight,
-}) => {
-	return (
-		<Carousel
-			cols={cols}
-			rows={rows}
-			loop
-			autoplay={autoplay}
-			gap={gap}
-			showDots={false}
-			arrowRight={({ onClick }) =>
-				arrowRight && <ArrowRight onClick={onClick}>&#10095;</ArrowRight>
-			}
-			arrowLeft={({ onClick }) =>
-				arrowLeft && <ArrowLeft onClick={onClick}>&#10094;</ArrowLeft>
-			}>
-			{children}
-		</Carousel>
-	);
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import CardMentor from "@/components/molecules/CardMentor";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { mentores } from "@/mockups/mentores";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const SliderComponent = () => {
+
+	const styleSlide = {
+		width: '10px',
+		marginRight:'0px',
+		marginLeft:'0px',
+		gap:'0px'
+	}
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={0}
+      slidesPerView={3}
+      navigation
+     
+    >
+      {mentores.map((mentor) => {
+        return (
+          <SwiperSlide style={styleSlide} key={mentor.name}>
+            <CardMentor mentor={mentor} />
+          </SwiperSlide>
+        );
+      })}
+      ...
+    </Swiper>
+  );
 };
 
 export default SliderComponent;
--- src/components/organisms/MentorSection/index.jsx
+++ src/components/organisms/MentorSection/index.jsx
@@ -1,39 +1,22 @@
-import React from 'react';
+import React from "react";
 import {
-	MentorsComponent,
-	MentorsContent,
-	SeeAll,
-	MentorsTitle,
-} from './style';
-import Carousel from 'better-react-carousel';
-import SliderComponent from '@/components/atoms/SliderComponent';
-import CardMentor from '@/components/molecules/CardMentor';
-import { mentores } from '@/mockups/mentores';
+  MentorsComponent,
+  MentorsContent,
+  SeeAll,
+  MentorsTitle,
+} from "./style";
+import SliderComponent from "@/components/atoms/SliderComponent";
 
 const MentorSection = () => {
-	return (
-		<MentorsComponent id='mentor'>
-			<MentorsContent>
-				<SeeAll>Ver todos</SeeAll>
-				<MentorsTitle>Encontre seu mentor</MentorsTitle>
-				<SliderComponent
-					cols={4}
-					row={1}
-					autoplay={2000}
-					arrowLeft={true}
-					arrowRight={true}
-					gap={'0px'}>
-					{mentores.map((mentor) => {
-						return (
-							<Carousel.Item key={mentor.name}>
-								<CardMentor mentor={mentor} />
-							</Carousel.Item>
-						);
-					})}
-				</SliderComponent>
-			</MentorsContent>
-		</MentorsComponent>
-	);
+  return (
+    <MentorsComponent id="mentor">
+      <MentorsContent>
+        <SeeAll>Ver todos</SeeAll>
+        <MentorsTitle>Encontre seu mentor</MentorsTitle>
+        <SliderComponent></SliderComponent>
+      </MentorsContent>
+    </MentorsComponent>
+  );
 };
 
 export default MentorSection;
