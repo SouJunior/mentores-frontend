8ed6de0bc0a61239069e7e22bd081a5310eca201
Luiz Bello <88148849+luizpbello@users.noreply.github.com>
2023-08-22 01:35:12 +0000

    refact: removendo console.log

--- src/services/usePasswordResetService.ts
+++ src/services/usePasswordResetService.ts
@@ -11,17 +11,13 @@ const usePasswordResetService = (): IUsePasswordService => {
   const sendResetLink = async (data: UserPasswordServiceDTO) => {
     try {
       const email = data.email;
-      const encodedEmail = encodeURIComponent(email);
-      console.log(email)
       const response = await axios.post(
         `https://mentores-backend.onrender.com/user/restoreAccount/${email}`,
       );
       if (response.status === 201) {
         setIsModalOpen(true);
         console.log("Email de recuperação enviado com sucesso!");
-      } else {
-        console.log("Algo deu errado ao enviar o email de recuperação.");
-      }
+      } 
     } catch (error) {
       console.log("Erro ao enviar o email de recuperação:", error);
     }
