94120a023ebd56773091b3176c5fd3ef12e956e4
Luiz Bello <88148849+luizpbello@users.noreply.github.com>
2023-10-19 19:34:13 +0000

    feat: pagina de FAQ

---
+++ src/styles/pages/faq.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+
+export const FaqContainer = styled.div `
+display:flex;
+justify-content: center;
+gap:74px;
+`
+
+export const ImageContainer = styled.div`
+margin-top: 80px;
+
+`
+export const AccordionContainer = styled.div`
+display:flex;
+flex-direction: column;
+
+`
+
+export const AccordionContent = styled.span`
+font-size: 16px;
+font-weight: 400;
+line-height: 22px;
+letter-spacing: 0em;
+text-align: left;
+
+`
+
+export const AccordionTitle = styled.span `
+font-size: 20px;
+font-weight: 500;
+line-height: 24px;
+letter-spacing: 0em;
+text-align: left;
+color: #002c66;;
+
+
+`
+
+export const TitleSpan = styled.span `
+color: #002c66;
+font-size: 32px;
+font-weight: 600;
+line-height: 38px;
+letter-spacing: 0em;
+text-align: left;
+
+
+`
