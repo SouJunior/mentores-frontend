dd6d9199c9be8ee10c1cce4e16ccd89136528a43
Raiane <raiane.oliveira404@gmail.com>
2023-11-16 20:39:15 -0300

    fix: add footer to mentors page and fix grid layout

--- src/styles/pages/mentors.ts
+++ src/styles/pages/mentors.ts
@@ -1,46 +1,45 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const MainContainer = styled.div`
-  background-color: #F1F3F5;
+  background-color: #f1f3f5;
   display: flex;
   flex-direction: column;
   margin-top: -80px;
   gap: 10px;
-`;
+`
 
 export const MentorsContainer = styled.div`
   display: grid;
-  place-items: center;
-  grid-template-columns: repeat(3, 1fr);
+  justify-content: center;
+  grid-template-columns: repeat(auto-fit, minmax(23.5rem, 1fr));
+  grid-auto-rows: max-content;
   grid-gap: 30px;
   width: 100vw;
   min-height: 100vh;
   padding: 20px 8%;
-  align-items: start;
   position: relative;
-`;
+`
 
 export const SubHeaderContainer = styled.div`
   display: flex;
   height: 100px;
   flex-direction: column;
   padding: 24px 120px;
-`;
+`
 
 export const TitleContainer = styled.span`
   padding: 5px 50px;
-  a{
+  a {
     color: #666666;
     font-weight: 400;
     font-size: 14px;
-    
   }
-`;
+`
 
 export const SubTitleContainer = styled.span`
   font-size: 14px;
   color: #003986;
-`;
+`
 
 export const CTAMain = styled.span`
   font-size: 32px;
@@ -49,12 +48,12 @@ export const CTAMain = styled.span`
   color: #003986;
   display: flex;
   flex-direction: column;
-`;
+`
 export const CTASub = styled.span`
   font-size: 16px;
   color: #666666;
   font-weight: 400;
-`;
+`
 
 export const NoResultContainer = styled.div`
   position: absolute;
@@ -65,22 +64,22 @@ export const NoResultContainer = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 3px;
-`;
+`
 
 export const NoResultMain = styled.span`
   font-size: 24px;
   font-weight: 600;
   color: #003986;
   margin-top: 10px;
-`;
+`
 
 export const StacksContainer = styled.span`
   display: flex;
   gap: 8px;
   width: 100%;
   flex-wrap: wrap;
-  padding:24px 0px 0px 170px;
-`;
+  padding: 24px 0px 0px 170px;
+`
 
 export const Stack = styled.span`
   background: #fff;
@@ -92,4 +91,4 @@ export const Stack = styled.span`
   padding: 8px;
   border-radius: 40px;
   text-align: center;
-`;
+`
