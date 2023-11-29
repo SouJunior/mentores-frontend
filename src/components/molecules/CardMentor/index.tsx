9dfd523305f30b3bab1f42d16b8e48fceefde7df
Patrick Otero <patrick.rocha.otero@gmail.com>
2023-11-29 07:17:25 -0300

    Mentor GET endpoint integration done

--- src/components/molecules/CardMentor/index.tsx
+++ src/components/molecules/CardMentor/index.tsx
@@ -1,5 +1,4 @@
 import { Card } from "@/components/atoms/Card";
-import { MentorType } from "@/mockups/mentores";
 import Image from "next/image";
 import {
   CardButton,
@@ -8,26 +7,29 @@ import {
   CardSubtitle,
   CardTitle,
 } from "./style";
+import { IMentors } from "@/services/interfaces/IUseMentorsService";
 
 interface CardMentorProps {
-  mentor: MentorType;
+  mentor: IMentors;
 }
 
 export function CardMentor({ mentor }: CardMentorProps) {
   return (
     <Card height={443} justifyContent={"center"}>
+      { mentor.profile &&
       <Image
-        src={mentor.image}
+        src={mentor.profile}
         width={150}
         height={150}
         style={{ borderRadius: "100%", objectFit: "cover" }}
-        alt={mentor.name}
+        alt={mentor.fullName}
       />
-      <CardTitle>{mentor.name}</CardTitle>
+      }
+      <CardTitle>{mentor.fullName}</CardTitle>
       <CardSubtitle>{mentor.role}</CardSubtitle>
       <CardStacks>
-        {mentor.stacks.map((stack) => (
-          <CardStack key={stack}>{stack}</CardStack>
+        {mentor.specialties.map((specialty: string) => (
+          <CardStack key={specialty}>{specialty}</CardStack>
         ))}
       </CardStacks>
       <CardButton>Agendar um horário</CardButton>
