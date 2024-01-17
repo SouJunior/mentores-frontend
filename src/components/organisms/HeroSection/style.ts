77a1793b13f8411e23b9cd90d0c9e1bf7faecb40
Raiane <raiane.oliveira404@gmail.com>
2024-01-17 11:31:09 -0300

    feat: search mentor form in homepage

--- src/components/organisms/HeroSection/style.ts
+++ src/components/organisms/HeroSection/style.ts
@@ -29,23 +29,36 @@ export const ContainerHero = styled.div`
   }
 `
 
-export const ContainerInput = styled.div`
+export const ContainerInputForm = styled.form`
   border-radius: 8px;
   box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.13);
   background-color: '#FAFAFA';
   width: 680px;
   border: 1px solid ${(props) => props.theme.colors.opacityGray};
-  padding: 0px 12px 0px 16px;
+  padding: 0.5rem 0.75rem;
   display: flex;
-  align-items: center;
   justify-content: space-between;
 
   div {
     display: flex;
     align-items: center;
-    justify-content: left;
-    gap: 16px;
-    width: 74%;
+    gap: 1rem;
+    flex: 1;
+
+    svg {
+      color: ${(props) => props.theme.colors.blue[400]};
+    }
+
+    input {
+      padding: 0.75rem 0;
+      padding-right: 1rem;
+      font-size: 1.125rem;
+      line-height: 1.7rem;
+
+      &::placeholder {
+        color: ${(props) => props.theme.colors.gray[500]};
+      }
+    }
   }
 `
 
@@ -54,4 +67,29 @@ export const TextAnimated = styled.h3`
   font-size: ${(props) => props.theme.fontSizes.xxl};
   position: absolute;
   margin-bottom: 40px;
+`
+
+export const ButtonMentor = styled.button`
+  display: block;
+  max-width: 13.5rem;
+  width: 100%;
+  padding: 0.75rem;
+
+  background-color: ${(props) => props.theme.colors.blue[400]};
+  color: ${(props) => props.theme.colors.white};
+  line-height: 1.5rem;
+  border-radius: 0.5rem;
+  border: 1px solid ${(props) => props.theme.colors.blue[400]};
+
+  text-align: center;
+  font-size: 1rem;
+
+  &:not(:disabled):hover {
+    background-color: #fff;
+    color: ${(props) => props.theme.colors.blue[400]};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+  }
 `
