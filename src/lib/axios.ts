fd25085f9fef80387dae2153d0600b0451a61855
Patrick Otero <83041269+PatrickOtero@users.noreply.github.com>
2024-01-18 13:48:19 -0300

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-// const serverUrl = 'https://mentores-backend.onrender.com'
-const localUrl = "http://localhost:3003"
+const serverUrl = 'https://mentores-backend.onrender.com'
+// const localUrl = "http://localhost:3003"
 
 export const api = axios.create({
-  baseURL: localUrl,
+  baseURL: serverUrl,
 })
