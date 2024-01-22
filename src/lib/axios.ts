ef8050a3384add8f5f1989bb907e3f6b35e47596
Raiane Oliveira <raiane.oliveira404@gmail.com>
2024-01-22 08:15:46 -0300

    fix: API url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const serverUrl = 'https://mentores-backend.onrender.com'
+const serverUrl = 'https://mentores-backend.soujunior.tech'
 
 export const api = axios.create({
   baseURL: serverUrl,
