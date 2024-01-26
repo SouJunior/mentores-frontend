5d2fc6ec07a211ef8be02e059c64c9c37aa2486a
Raiane <raiane.oliveira404@gmail.com>
2024-01-26 10:34:36 -0300

    refactor: modal validate email styles

--- src/components/molecules/ModalEmail/index.tsx
+++ src/components/molecules/ModalEmail/index.tsx
@@ -2,7 +2,14 @@ import { Modal } from '@/components/atoms/Modal'
 import Image from 'next/image'
 import confirmEmail from '@/assets/ConfirmEmail.png'
 import logo from '@/assets/logos/sou-junior.svg'
-import { ContainerModal, FooterModal, Hash, Message, TitleModal } from './style'
+import {
+  ContainerModal,
+  FooterModal,
+  Hash,
+  HeaderModal,
+  Message,
+  TitleModal,
+} from './style'
 
 interface ModalEmailProps {
   open: boolean
@@ -14,14 +21,18 @@ export default function ModalEmail({ open, onClose, height }: ModalEmailProps) {
   return (
     <Modal open={open} onClose={onClose} height={height} bgColor="#fff">
       <ContainerModal>
-        <TitleModal>Cheque seu email</TitleModal>
-        <Image
-          src={confirmEmail}
-          alt="Garoto mexendo em nootebook"
-          width={285}
-          height={278}
-        />
-        <Message>Enviamos um email para você de confirmação.</Message>
+        <HeaderModal>
+          <TitleModal>Cheque seu e-mail</TitleModal>
+          <Image
+            src={confirmEmail}
+            alt="Garoto mexendo em nootebook"
+            width={285}
+            height={278}
+          />
+
+          <Message>Enviamos um email para você de confirmação.</Message>
+        </HeaderModal>
+
         <FooterModal>
           <Image src={logo} alt="logo da SouJunior" width={108} height={17} />
           <Hash>#MovimentoSouJunior</Hash>
