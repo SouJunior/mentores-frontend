87d7e2dd91a3cd47fb140ff6501d6e9518c94787
Raiane <raiane.oliveira404@gmail.com>
2024-01-30 11:11:10 -0300

    feat: new colors theme and fix global styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,33 +1,49 @@
 export const colors = {
+  white: '#fff',
+  gray: {
+    50: '#FDFDFD',
+    200: '#DEDEDE',
+    250: '#D9D9D9',
+    500: '#C5C7C5',
+    600: '#ACACAC',
+    700: '#666666',
+    750: '#5D5F5D',
+  },
+  black: {
+    200: '#323232',
+    500: '#212121',
+  },
   blue: {
+    25: '#F1F3F5',
     300: '#95C6FF',
     400: '#046AD0',
     500: '#1165BA',
-    600: '#338AFF',
-    700: '#003986',
+    600: '#175CB7',
+    700: '#338AFF',
+    800: '#003986',
+    950: '#001633',
   },
-  gray1: {
-    neutral: ' #DEDEDE',
+  red: {
+    25: '#F5F1F3',
+    300: '#db2e34',
+    500: '#E94242',
+    600: '#C1292E',
+    800: '#8f1e22',
   },
-  gradient:
-    'linear-gradient(303.77deg, rgba(17, 101, 186, 0.79) 34.68%, rgba(90, 156, 255, 0.79) 79.96%), linear-gradient(303.77deg, rgba(217, 217, 217, 0) -1.3%, rgba(17, 101, 186, 0.79) 54.57%), linear-gradient(180deg, rgba(217, 217, 217, 0) 0%, rgba(17, 101, 186, 0.79) 68.75%)',
-  black: '#212121',
-  white: '#FDFDFD',
-  red: { 500: '#C1292E', 300: '#db2e34', 700: '#8f1e22' },
-  gray: { 300: '#D7D9D7', 500: '#C5C7C5', 700: '#5D5F5D' },
-  opacityGray: 'rgba(197, 199, 197, 0.56)',
-  placeholder: '#C5C7C5',
-  error: '#E94242',
+  gradient: {
+    primary:
+      'linear-gradient(303.77deg, rgba(17, 101, 186, 0.79) 34.68%, rgba(90, 156, 255, 0.79) 79.96%), linear-gradient(303.77deg, rgba(217, 217, 217, 0) -1.3%, rgba(17, 101, 186, 0.79) 54.57%), linear-gradient(180deg, rgba(217, 217, 217, 0) 0%, rgba(17, 101, 186, 0.79) 68.75%)',
+  },
 } as const
 
 export const fontSizes = {
-  xxxl: '48px',
-  xxl: '40px',
-  xl: '32px',
-  lg: '24px',
-  md: '18px',
-  sm: '16px',
-  xs: '14px',
+  xxxl: '3rem',
+  xxl: '2.5rem',
+  xl: '2rem',
+  lg: '1.5rem',
+  md: '1.125rem',
+  sm: '1rem',
+  xs: '0.875rem',
 } as const
 
 export const theme = {
