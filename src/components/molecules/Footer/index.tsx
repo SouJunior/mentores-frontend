a3376e6103ee91d2125048e472ef2198305a6f41
Raiane <raiane.oliveira404@gmail.com>
2024-01-31 00:05:37 -0300

    feat: new footer styles

--- src/components/molecules/Footer/index.tsx
+++ src/components/molecules/Footer/index.tsx
@@ -1,27 +1,33 @@
-import { Button } from '@/components/atoms/Button'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 import { ModalPrivacyPolicy } from '../ModalPrivacyPolicy'
 import ModalTerms from '../ModalTerms'
+
 import {
-  ContainerFooter,
-  ContainerModais,
-  ContainerSocialMedias,
-  SectionFooter,
-  SectionFooterLinks,
+  FooterBtn,
+  FooterContainer,
+  FooterContent,
+  FooterCopyright,
+  FooterLink,
+  FooterMainInfo,
+  FooterSitemap,
+  FooterSocialMediaContainer,
+  FooterTitle,
+  FooterTitleContainer,
 } from './style'
 
-import discord from '@/assets/homepage/footer/discord.webp'
-import facebook from '@/assets/homepage/footer/face.webp'
-import github from '@/assets/homepage/footer/github.webp'
-import insta from '@/assets/homepage/footer/insta.webp'
-import linkedin from '@/assets/homepage/footer/linkedin.webp'
-import telegram from '@/assets/homepage/footer/telegram.webp'
-import twitch from '@/assets/homepage/footer/twitch.webp'
-import twitter from '@/assets/homepage/footer/twitter.webp'
-import youtube from '@/assets/homepage/footer/youtube.webp'
 import logo from '@/assets/logos/sou-junior-black.webp'
+import {
+  DiscordLogo,
+  FacebookLogo,
+  GithubLogo,
+  InstagramLogo,
+  LinkedinLogo,
+  TelegramLogo,
+  TwitchLogo,
+  YoutubeLogo,
+} from 'phosphor-react'
 
 export function Footer() {
   const [openTermos, setOpenTermos] = useState(false)
@@ -33,119 +39,147 @@ export function Footer() {
   const handleClosePoliticas = () => setOpenPoliticas(false)
 
   return (
-    <footer>
-      <ContainerFooter>
-        <SectionFooter>
-          <Image src={logo} alt="Logo" width={260} height={40} />
-          <ContainerModais>
-            <Button
-              content={'Termos de uso'}
-              btnRole={'unstyled'}
-              onClick={handleOpenTermos}
-            />
+    <FooterContainer>
+      <FooterContent className="container">
+        <FooterMainInfo>
+          <Image
+            src={logo}
+            alt="SouJunior logo"
+            width={248}
+            height={40}
+            quality={100}
+          />
 
-            <span />
-            <Button
-              content={'Políticas de Privacidade'}
-              btnRole={'unstyled'}
-              onClick={handleOpenPoliticas}
-            />
-          </ContainerModais>
-          <ModalTerms
-            open={openTermos}
-            onClose={handleCloseTermos}
-            height={590}
-            width={600}
-          />
-          <ModalPrivacyPolicy
-            open={openPoliticas}
-            onClose={handleClosePoliticas}
-            height={590}
-            width={600}
-          />
-        </SectionFooter>
-        <SectionFooterLinks>
-          <h2>Seja mentorado</h2>
-          <Link href="/#mentor">Encontre seu mentor</Link>
-          <Link href="/#onboarding">Como funciona</Link>
-        </SectionFooterLinks>
-        <SectionFooterLinks>
-          <h2>Mentore</h2>
-          <Link href="/cadastro">Cadastrar</Link>
-          <Link href="/login">Entrar</Link>
-        </SectionFooterLinks>
-        <SectionFooterLinks>
-          <h2>Suporte</h2>
-          <Link href="/faq">FAQ</Link>
-        </SectionFooterLinks>
-      </ContainerFooter>
-      <ContainerSocialMedias>
-        <Link
-          href={'https://www.linkedin.com/company/soujunior/'}
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          <Image width={40} height={40} src={linkedin} alt="logo Linkedin" />
-        </Link>
-        <Link
-          href={'https://github.com/SouJunior'}
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          <Image width={40} height={40} src={github} alt="logo github" />
-        </Link>
-        <Link
-          href={
-            'https://discord.com/invite/soujunior-community-759176734460346423'
-          }
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          <Image width={40} height={40} src={discord} alt="logo discord" />
-        </Link>
-        <Link
-          href={'https://www.youtube.com/@soujuniortech'}
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          <Image width={40} height={40} src={youtube} alt="logo youtube" />
-        </Link>{' '}
-        <Link
-          href={'https://twitter.com/SouJunior_Tech'}
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          <Image width={40} height={40} src={twitter} alt="logo twitter" />
-        </Link>{' '}
-        <Link
-          href={'https://www.instagram.com/soujunior.tech/'}
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          <Image width={40} height={40} src={insta} alt="logo instagram" />
-        </Link>{' '}
-        <Link
-          href={'https://www.facebook.com/people/SouJunior/100086671131030'}
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          <Image width={40} height={40} src={facebook} alt="logo facebook" />
-        </Link>{' '}
-        <Link
-          href={'https://t.me/soujuniortech'}
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          <Image width={40} height={40} src={telegram} alt="logo telegram" />
-        </Link>{' '}
-        <Link
-          href={'https://www.twitch.tv/soujuniortech'}
-          target="_blank"
-          referrerPolicy="no-referrer"
-        >
-          <Image width={40} height={40} src={twitch} alt="logo twitch" />
-        </Link>
-      </ContainerSocialMedias>
-    </footer>
+          <FooterSitemap>
+            <FooterTitleContainer>
+              <FooterTitle>Tenha mentorias</FooterTitle>
+              <FooterLink href="/#onboarding">Como funciona</FooterLink>
+            </FooterTitleContainer>
+
+            <FooterTitleContainer>
+              <FooterTitle>Mentores</FooterTitle>
+              <FooterLink href="/cadastro">Cadastrar</FooterLink>
+              <FooterLink href="/login">Login</FooterLink>
+            </FooterTitleContainer>
+
+            <FooterTitleContainer>
+              <FooterTitle>Suporte</FooterTitle>
+              <FooterLink href="/faq">FAQ</FooterLink>
+            </FooterTitleContainer>
+
+            <FooterTitleContainer>
+              <FooterTitle>Legal</FooterTitle>
+              <FooterBtn onClick={handleOpenTermos}>Termo de uso</FooterBtn>
+              <FooterBtn onClick={handleOpenPoliticas}>
+                Política de privacidade
+              </FooterBtn>
+
+              <ModalTerms
+                open={openTermos}
+                onClose={handleCloseTermos}
+                height={590}
+                width={600}
+              />
+              <ModalPrivacyPolicy
+                open={openPoliticas}
+                onClose={handleClosePoliticas}
+                height={590}
+                width={600}
+              />
+            </FooterTitleContainer>
+          </FooterSitemap>
+        </FooterMainInfo>
+
+        <FooterSocialMediaContainer>
+          <Link
+            target="_blank"
+            referrerPolicy="no-referrer"
+            href="https://www.linkedin.com/company/soujunior/"
+            title="Linkedin"
+          >
+            <LinkedinLogo weight="bold" />
+          </Link>
+          <Link
+            target="_blank"
+            referrerPolicy="no-referrer"
+            href="https://github.com/SouJunior"
+            title="Github"
+          >
+            <GithubLogo weight="bold" />
+          </Link>
+          <Link
+            target="_blank"
+            referrerPolicy="no-referrer"
+            href="https://discord.com/invite/soujunior-community-759176734460346423"
+            title="Discord"
+          >
+            <DiscordLogo weight="bold" />
+          </Link>
+          <Link
+            target="_blank"
+            referrerPolicy="no-referrer"
+            href="https://www.youtube.com/@soujuniortech"
+            title="Youtube"
+          >
+            <YoutubeLogo weight="bold" />
+          </Link>
+          <Link
+            target="_blank"
+            referrerPolicy="no-referrer"
+            href="https://twitter.com/SouJunior_Tech"
+            title="Twitter"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M16.7447 4.5H19.2748L13.7473 10.8176L20.25 19.4145H15.1584L11.1705 14.2005L6.60746 19.4145H4.07582L9.98808 12.6571L3.75 4.5H8.97083L12.5755 9.26575L16.7447 4.5ZM15.8567 17.9001H17.2587L8.20905 5.93485H6.7046L15.8567 17.9001Z"
+                fill="white"
+              />
+            </svg>
+          </Link>
+          <Link
+            target="_blank"
+            referrerPolicy="no-referrer"
+            href="https://www.instagram.com/soujunior.tech/"
+            title="Instagram"
+          >
+            <InstagramLogo weight="bold" />
+          </Link>
+          <Link
+            target="_blank"
+            referrerPolicy="no-referrer"
+            href="https://www.facebook.com/people/SouJunior/100086671131030"
+            title="Facebook"
+          >
+            <FacebookLogo weight="bold" />
+          </Link>
+          <Link
+            target="_blank"
+            referrerPolicy="no-referrer"
+            href="https://t.me/soujuniortech"
+            title="Telegram"
+          >
+            <TelegramLogo weight="bold" />
+          </Link>
+          <Link
+            target="_blank"
+            referrerPolicy="no-referrer"
+            href="https://www.twitch.tv/soujuniortech"
+            title="Twitch"
+          >
+            <TwitchLogo weight="bold" />
+          </Link>
+        </FooterSocialMediaContainer>
+
+        <FooterCopyright>
+          © 2023 SouJunior. Todos os direitos reservados.
+        </FooterCopyright>
+      </FooterContent>
+    </FooterContainer>
   )
 }
--- src/components/molecules/Footer/style.ts
+++ src/components/molecules/Footer/style.ts
@@ -1,70 +1,98 @@
+import { device } from '@/styles/theme'
+import Link from 'next/link'
 import styled from 'styled-components'
 
-export const ContainerFooter = styled.div`
-  padding: 48px;
-  display: flex;
-  justify-content: space-around;
-  gap: 48px;
-  background: #5d5f5d;
-  color: #fff;
-  font-size: 16px;
+export const FooterContainer = styled.footer`
+  background-color: ${(props) => props.theme.colors.gray[700]};
+  color: ${(props) => props.theme.colors.white};
 `
 
-export const ContainerModais = styled.div`
-  display: flex;
-  justify-content: left;
-  align-items: left;
-  gap: 24px;
-
-  span {
-    width: 1px;
-    height: 24px;
-    background-color: #fff;
-  }
-`
-
-export const ContainerSocialMedias = styled.div`
-  background: #5d5f5d;
-  color: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 64px;
-  padding: 0px 0px 64px;
-
-  a {
-    &:hover {
-      filter: drop-shadow(0px 0px 20px rgba(0, 0, 0, 0.6));
-    }
-  }
-`
-
-export const SectionFooter = styled.section`
+export const FooterContent = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 2.5rem;
+
+  padding: 4rem 2rem;
+
+  @media ${device.desktopS} {
+    padding: 4rem 1rem;
+  }
+
+  @media ${device.mobileL} {
+    padding: 2rem 1rem;
+  }
+`
+
+export const FooterMainInfo = styled.div`
+  display: flex;
   justify-content: space-between;
-  align-items: center;
-  max-width: fit-content;
-  max-height: 120px;
+  align-items: flex-start;
+  gap: 2rem;
+  flex-wrap: wrap;
 `
 
-export const SectionFooterLinks = styled.section`
+export const FooterSitemap = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  column-gap: 5rem;
+  row-gap: 2rem;
+
+  @media ${device.mobileL} {
+    flex-direction: column;
+  }
+`
+
+export const FooterTitleContainer = styled.section`
   display: flex;
   flex-direction: column;
-  justify-content: left;
-  align-items: left;
-  max-width: fit-content;
+  gap: 1.5rem;
+`
 
-  h2 {
-    font-size: 16px;
-    margin-bottom: 32px;
-    line-height: 20px;
+export const FooterLink = styled(Link)`
+  font-size: 1rem;
+  line-height: 1.4rem;
+  font-weight: 400;
+`
+
+export const FooterBtn = styled.button`
+  all: unset;
+  font-size: 1rem;
+  line-height: 1.4rem;
+  cursor: pointer;
+`
+
+export const FooterTitle = styled.h6`
+  font-size: 1.25rem;
+  font-weight: 500;
+  line-height: 1.5rem;
+`
+
+export const FooterSocialMediaContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  @media ${device.mobileL} {
+    gap: 0.5rem;
+    justify-content: center;
   }
 
-  a {
-    color: #fff;
-    text-align: justify;
-    margin-bottom: 24px;
-    line-height: 24px;
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+`
+
+export const FooterCopyright = styled.p`
+  padding-top: 1rem;
+  border-top: 1px solid ${(props) => props.theme.colors.gray[200]};
+
+  font-size: ${(props) => props.theme.fontSizes.xs};
+  line-height: 1.25rem;
+  text-align: right;
+
+  @media ${device.mobileL} {
+    text-align: left;
   }
 `
