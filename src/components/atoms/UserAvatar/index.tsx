49a1a4a4dad61bf8164f2950d1ee31a4a6d0c072
Raiane <raiane.oliveira404@gmail.com>
2024-02-14 13:39:02 -0300

    fix: add fixed width and height styles to user avatar compoennt

--- src/components/atoms/UserAvatar/index.tsx
+++ src/components/atoms/UserAvatar/index.tsx
@@ -1,37 +1,22 @@
 import UserDefault from '@/assets/userDefault.png'
 import Image from 'next/image'
 import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined'
-import { useState } from 'react'
 import { useAuthContext } from '@/context/Auth/AuthContext'
+import { ImageContainer } from './styles'
 
 export function UserAvatar() {
-  const { userSession, mentor } = useAuthContext()
-  const [imageError, setImageError] = useState(false)
+  const { mentor } = useAuthContext()
 
   return (
     <>
-      {userSession ? (
-        userSession.token ? (
-          <div>
-            {imageError ? (
-              <Image alt="User Avatar" src={UserDefault} />
-            ) : (
-              <Image
-                width={44}
-                height={44}
-                alt="User Avatar"
-                style={{ border: '2px solid #666' }}
-                src={mentor.data?.profile ?? ''}
-                onError={() => setImageError(true)}
-              />
-            )}
-          </div>
-        ) : (
-          <Image alt="User Avatar" src={UserDefault} />
-        )
-      ) : (
-        <Image alt="User Avatar" src={UserDefault} />
-      )}
+      <ImageContainer>
+        <Image
+          width={44}
+          height={44}
+          alt="User Avatar"
+          src={mentor.data?.profile ?? UserDefault}
+        />
+      </ImageContainer>
 
       <ArrowBackIosOutlinedIcon />
     </>
