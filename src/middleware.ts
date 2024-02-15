f0cc8ce83be3ad72a8f67d3ec18492af98811af9
Raiane <raiane.oliveira404@gmail.com>
2024-02-15 11:33:48 -0300

    feat: add middleware in routes where only unauthenticated user can access

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { sessionNameUserInfo } from './data/static-info'
 
+// routes where only unauthenticated user can access
+const authRoutes = ['/login', '/cadastro']
+
 export function middleware(request: NextRequest) {
   const hasAuthToken = request.cookies.has(sessionNameUserInfo)
 
   if (!hasAuthToken) {
     return NextResponse.redirect(new URL('/', request.url))
   }
+
+  if (authRoutes.includes(request.nextUrl.pathname) && hasAuthToken) {
+    return NextResponse.redirect(new URL('/me', request.url))
+  }
 }
 
 export const config = {
-  matcher: ['/onBoarding'],
+  matcher: ['/onBoarding', '/login', '/cadastro'],
 }
