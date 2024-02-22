783b3793445db81b04dd29ba81aaa9133c509bd7
Raiane <raiane.oliveira404@gmail.com>
2024-02-22 10:52:23 -0300

    refactor: remove unecessary console logs

--- src/services/user/usePasswordResetService.ts
+++ src/services/user/usePasswordResetService.ts
@@ -3,15 +3,9 @@ import { api } from '@/lib/axios'
 
 const usePasswordResetService = () => {
   const sendResetLink = async (data: UserPasswordServiceDTO) => {
-    try {
-      const email = data.email
-      const response = await api.post(`/mentor/restoreAccount/${email}`)
-      if (response.status === 201) {
-        console.log('Email de recuperação enviado com sucesso!')
-      }
-    } catch (error) {
-      console.log('Erro ao enviar o email de recuperação:', error)
-    }
+    const email = data.email
+    const response = await api.post(`/mentor/restoreAccount/${email}`)
+    return response
   }
 
   return { sendResetLink }
