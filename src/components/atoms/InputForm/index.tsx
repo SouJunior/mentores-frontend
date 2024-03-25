a2dbfac66d7b7fae7896bad10c5fe0e04008633c
Raiane Oliveira <raiane.oliveira404@gmail.com>
2024-03-25 20:02:01 -0300

    Update src/components/atoms/InputForm/index.tsx
    Co-authored-by: Mario Paglia <mpagliajr@gmail.com>

--- src/components/atoms/InputForm/index.tsx
+++ src/components/atoms/InputForm/index.tsx
@@ -35,7 +35,7 @@ export function InputForm({
 
   return (
     <ContainerInputLabel>
-      <StyledLabel className={disabled ? 'disabled' : ''}>
+      <StyledLabel className={disabled && 'disabled'}>
         {label} {isRequired && <span className="asterisk">*</span>}
       </StyledLabel>
       <ContainerInput
