9526f24af2d630fc381b27b8b5f1e047f454dd9a
Hudson Santos <work.of.hudson@gmail.com>
2024-06-05 18:55:10 -0300

    feat: add image to the modal step 4

--- src/components/atoms/ModalCalendlyStep4/index.tsx
+++ src/components/atoms/ModalCalendlyStep4/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Button } from '@/components/atoms/Button'
 import { ModalButton, ModalDescription, ModalTitle } from '@/styles/pages/home'
+import Image from 'next/image'
+import imagemParabens from '@/assets/modalCalendly/parabens.png'
 
 type ModalCalendlyStep4Props = {
   handleCloseModal: () => void
@@ -19,6 +21,11 @@ export default function ModalCalendlyStep4({
       <ModalDescription>
         Seu cadastro como mentor foi concluído com sucesso.
       </ModalDescription>
+      <Image
+        alt="Imagem de uma mulher comemorando com um dos braços erguido"
+        src={imagemParabens}
+      />
+
       <Button as={ModalButton} onClick={handleCloseFromChild}>
         Fechar
       </Button>
