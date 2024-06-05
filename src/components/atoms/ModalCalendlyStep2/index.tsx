3670482b7fa0e4a7feba3f91dbfb6a955ca2e543
Hudson Santos <work.of.hudson@gmail.com>
2024-06-05 18:52:03 -0300

    feat: add more features in the modal step 3

--- src/components/atoms/ModalCalendlyStep2/index.tsx
+++ src/components/atoms/ModalCalendlyStep2/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Button } from '@/components/atoms/Button'
 import { ModalButton, ModalDescription, ModalTitle } from '@/styles/pages/home'
+import Image from 'next/image'
+import copyLinkEvent from '@/assets/modalCalendly/copyLinkEvent.png'
+import Link from 'next/link'
 
-export type ModalCalendlyStep2Props = {
+export interface ModalCalendlyStep2Props {
   handleNextStep: () => void
   handlePreviousStep: () => void
 }
@@ -25,10 +28,37 @@ export default function ModalCalendlyStep2({
         Acesse sua conta no Calendly e copie o seu link, conforme exemplificado
         abaixo.
       </ModalDescription>
-      <Button as={ModalButton} onClick={handleNextStepFromChild}>
+      <Image
+        alt="Imagem de um evento dentro do Calendly com o mouse apontando para o botão nomeado de copiar link"
+        src={copyLinkEvent}
+      />
+      <p>
+        Ou acesse esse{' '}
+        <Link
+          href="https://help.calendly.com/hc/en-us/articles/223193448-Sharing-your-scheduling-link#sharing-your-scheduling-link-0-0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          tutorial sobre compartilhamento de links do Calendly
+        </Link>{' '}
+        para uma explicação detalhada.
+      </p>
+
+      <Button
+        as={Link}
+        href="https://calendly.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={handleNextStepFromChild}
+      >
         Ir para o Calendly
       </Button>
-      <Button as={ModalButton} onClick={handlePreviousStepFromChild}>
+
+      <Button
+        as={ModalButton}
+        onClick={handlePreviousStepFromChild}
+        variant="secondary"
+      >
         Voltar
       </Button>
     </>
