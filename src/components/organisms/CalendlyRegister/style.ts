44136e316a6945bf5e8ad789c5648b9fd47052cf
Hudson Santos <work.of.hudson@gmail.com>
2024-06-10 17:26:58 -0300

    style: create parent style for the modals steps

--- src/components/organisms/CalendlyRegister/style.ts
+++ src/components/organisms/CalendlyRegister/style.ts
@@ -2,34 +2,60 @@ import { Modal } from '@/components/atoms/Modal'
 import styled from 'styled-components'
 
 export const ModalContainer = styled(Modal.Content)`
-  display: grid;
-  gap: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
   padding: 1.5rem;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
   position: relative;
+  max-width: 32rem;
 `
 
 export const ModalTitle = styled(Modal.Title)`
-  font-size: 1.5rem;
+  font-size: 1.4rem;
   color: ${(props) => props.theme.colors.black[200]};
   font-weight: 600;
   line-height: 140%;
   text-align: center;
-  max-width: 18.75rem;
-  margin: 0 auto;
+  padding: 0 4rem;
+  margin: 1.5rem auto 0 auto;
 `
 
 export const ModalDescription = styled(Modal.Description)`
-  font-size: 1.25rem;
+  font-size: 1.1rem;
   color: ${(props) => props.theme.colors.black[200]};
   line-height: 140%;
   font-family: 'Radio Canada', sans-serif;
   text-align: center;
-  max-width: 26.875rem;
+  padding: 0 4rem;
+`
+export const ModalImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
 `
 
 export const ModalButton = styled.button`
-  line-height: 1.2rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+`
+export const ModalButtonSecondary = styled(ModalButton)`
+  border: 2.5px solid ${(props) => props.theme.colors.gray[700]};
+  background-color: ${(props) => props.theme.colors.white};
+  color: ${(props) => props.theme.colors.gray[700]};
+
+  &:hover {
+    border: 2.5px solid ${(props) => props.theme.colors.gray[750]} !important;
+    background-color: ${(props) => props.theme.colors.gray[200]} !important;
+    color: ${(props) => props.theme.colors.gray[750]} !important;
+  }
 `
 
 export const ModalClose = styled(Modal.Close)`
