a7133718a28c1a7f1991049103ab0be8a2b7b6f6
Hudson Santos <work.of.hudson@gmail.com>
2024-06-10 17:40:31 -0300

    style: stepper dots new style

--- src/components/atoms/StepperDots/style.ts
+++ src/components/atoms/StepperDots/style.ts
@@ -4,15 +4,19 @@ import { MovementAnimatorProps } from '.'
 export const ContainerStepper = styled.div`
   display: flex;
   flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 1.5rem 0.55rem;
 `
 
 export const CircleStepper = styled.ul`
-  width: 40px;
-  height: 40px;
+  width: 11px;
+  height: 11px;
   background-color: #ddd;
   position: relative;
   border-radius: 20px;
-  margin-right: 4px;
+  margin-right: 9px;
+  margin-left: 9px;
   overflow: hidden;
 `
 
@@ -20,10 +24,10 @@ export const MovementIndicators = styled.div`
   position: absolute;
   left: 0;
   top: 0;
-  width: 40px;
-  height: 40px;
+  width: 11px;
+  height: 11px;
   border-radius: 20px;
-  background-color: tomato;
+  background-color: ${(props) => props.theme.colors.blue[800]};
   transition: transform 0.5s ease;
 `
 
