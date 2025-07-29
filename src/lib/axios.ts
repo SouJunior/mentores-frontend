9df4f7643c0cd31fb0c29ec91ba4cbe6a46c9da2
Patrick Otero <83041269+PatrickOtero@users.noreply.github.com>
2025-07-29 18:47:11 -0300

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const serverUrl = {
     development: 'http://localhost:3000',
 
-  production: 'https://p01--mentores-backend-api--brg9tw85vflp.code.run/',
+  production: 'https://p01--mentores-backend-api--brg9tw85vflp.code.run',
 };
 
 export const api = axios.create({
