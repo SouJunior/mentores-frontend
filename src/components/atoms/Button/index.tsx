8b87fce3dcfcd5ea1ac4c58237cdf5e3b01059f5
Patrick Otero <patrick.rocha.otero@gmail.com>
2026-05-15 23:48:49 -0300

    fix: usa props transientes em botões estilizados

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -2,9 +2,13 @@ import { ComponentType, ElementType, ReactNode } from 'react';
 import { ButtonStyle, ButtonStyleProps } from './style';
 
 type ElementOrComponentType = ElementType<any> | ComponentType<any>;
+type ButtonVariant = NonNullable<ButtonStyleProps['$variant']>;
+type ButtonSize = NonNullable<ButtonStyleProps['$size']>;
 
-interface ButtonProps extends ButtonStyleProps {
+interface ButtonProps {
   children: ReactNode;
+  variant?: ButtonVariant;
+  size?: ButtonSize;
   as?: ElementOrComponentType;
   [key: string]: any;
 }
@@ -16,7 +20,7 @@ export function Button({
   ...props
 }: ButtonProps) {
   return (
-    <ButtonStyle variant={variant} size={size} {...props}>
+    <ButtonStyle $variant={variant} $size={size} {...props}>
       {children}
     </ButtonStyle>
   );
